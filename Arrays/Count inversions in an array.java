dc4f1e7554695daa3c8b52938c6cc39465955c42
saurav40 <42322664+saurav40@users.noreply.github.com>
2022-07-21 01:26:13 +0530

    Create Count inversions in an array.java

---
+++ Arrays/Count inversions in an array.java
@@ -0,0 +1,56 @@
+  Class solution{
+    int total;
+    int tmp[];
+    public int inversion(int[] nums) {
+        total=0;
+        tmp=new int[nums.length];
+        merge_sort(nums,0,nums.length-1);
+        return total;
+    }
+    public void merge_sort(int [] nums,int left,int right)
+    {
+        if(right>left)
+        {
+            int mid=(left+right)/2;
+            
+            merge_sort(nums,left,mid);
+            merge_sort(nums,mid+1,right);
+            
+            merge(nums,left,mid,right,tmp);
+        }
+    }
+    
+    public void merge(int[]nums,int left,int mid,int right,int []tmp)
+    {
+      
+       int i=left;
+       int j=mid+1;
+        int k=left;
+        
+        while(i<=mid && j<=right)
+        {
+            if(nums[i]<=nums[j])
+            {
+                tmp[k++]=nums[i++];
+            }
+            else
+            {
+                tmp[k++]=nums[j++];
+                total+=mid+1-i;
+            }
+                
+        }
+        
+        while(i<=mid)
+        {
+            tmp[k++]=nums[i++];
+        }
+        while(j<=right)
+        {
+            tmp[k++]=nums[j++];
+        }
+        for(i = left ; i <= right ; i++)
+               nums[i] = tmp[i];
+    }
+  }
+
